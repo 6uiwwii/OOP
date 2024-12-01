8119b902104a48416034050bb319e6cbb72396cc
uiwwii <67070283@kmitl.ac.th>
2024-12-01 21:47:58 +0700

    lab1

---
+++ week1/src/CircleArea.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author computer
+ */
+import java.util.*;
+public class CircleArea {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double radius = input.nextDouble(),area;
+        area = Math.PI * radius * radius ;
+        System.out.println(area);
+    }
+    
+}
